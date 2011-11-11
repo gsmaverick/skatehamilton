89660863a8156e7cc5adc70dd43d6f81ab2fe31b
gsmaverick <gavin.schulz@gmail.com>
2011-11-11 17:13:17 -0500

    Ready for heroku with nothing in it yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skatehamilton::Application.config.secret_token = '62af2cc139abac817f6ceb9723838859deaaecffa6b042b0d4ef0b2e7b7a8ccdd7741b59e15320e9afdce6fc83b72da2587688b3c83a1624c62a5313e626ab66'
