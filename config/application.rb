091f5a4f929a359880137bf66e8740865cae58a8
gsmaverick <gavin.schulz@gmail.com>
2011-11-12 19:25:12 -0500

    Maybe this time

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,5 @@ module Skatehamilton
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.precompile += ['main.css']
   end
 end
