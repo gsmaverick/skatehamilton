3893971443265222c4633d75b00d8849821928c9
gsmaverick <gavin.schulz@gmail.com>
2011-11-24 16:29:51 -0500

    Mostly visual changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,11 @@ class HomeController < ApplicationController
   def index
     @Rinks = Rink.all(:select => 'id, address, latitude, longitude, name, rink_type').to_json
 
-    if isMobile
-      render "home/mobile/index", :layout => "mobile"
-    else
-      render "home/desktop/index", :layout => "desktop"
-    end
+    #if isMobile
+    render "home/mobile/index", :layout => "mobile"
+    #else
+    #  render "home/desktop/index", :layout => "desktop"
+    #end
   end
 
   def about
